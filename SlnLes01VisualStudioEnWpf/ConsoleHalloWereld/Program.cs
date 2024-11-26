688952af9348f8ad2102783e838eff852421d637
YoussAfk <youssef.afkir@student.odisee.be>
2024-11-26 13:37:37 +0100

    hello world

---
+++ SlnLes01VisualStudioEnWpf/ConsoleHalloWereld/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleHalloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Wereld!");
+            Console.Write("Hoe heet je: ");
+            string naam = Console.ReadLine();
+            Console.WriteLine($"Aangename kennismaking, {(naam == "Youssef" ? "Youssef" : "onbekende")}.");
+            Console.WriteLine("Druk een toets om verder te gaan...");
+            Console.ReadKey();
+        }
+    }
+}
