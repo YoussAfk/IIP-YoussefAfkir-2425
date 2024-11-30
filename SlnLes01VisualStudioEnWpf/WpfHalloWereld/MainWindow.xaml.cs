197aa4e199b75ab6183e6010436df66a7f9a1cf3
YoussAfk <youssef.afkir@student.odisee.be>
2024-11-30 21:59:17 +0100

    hello world

---
+++ SlnLes01VisualStudioEnWpf/WpfHalloWereld/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHalloWereld
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            txtHello.Text = "Hallo " + txtNaam.Text;
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            txtHello.Text = "Hoe is je naam?";
+            txtNaam.Text = "";
+        }
+
+        private void BtnSayHello_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
