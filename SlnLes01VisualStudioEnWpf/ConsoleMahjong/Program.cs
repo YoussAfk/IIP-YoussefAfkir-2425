afca0ad40883e95744369c2b486b04b8ed816e7b
YoussAfk <youssef.afkir@student.odisee.be>
2024-12-03 16:29:29 +0100

    ConsoleMahjong

---
+++ SlnLes01VisualStudioEnWpf/ConsoleMahjong/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace les_1_opgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string header =
+            (@"************************
+|  WELKOM BIJ MAHJONG  |
+************************");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(header);
+
+            Console.WriteLine();
+
+            // resetcolor
+            Console.ResetColor();
+            Console.Write("Druk op een");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" willekeurige toets ");
+            Console.ResetColor();
+            Console.WriteLine("om de steen te tonen...");
+            Console.ReadKey(true);
+
+            // utf-8 karakterset
+            //color
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" ◉◉◉ ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ◉◉◉ ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" ◉◉◉ ");
+            Console.ResetColor();
+
+            //afsluiten 
+            Console.Write("druk op een toets om af te sluiten...");
+
+
+
+        }
+    }
+}
