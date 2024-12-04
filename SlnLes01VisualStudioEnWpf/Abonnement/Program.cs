f487788a7a58efb9fe9d3c985d2df82ac1fd6d0b
YoussAfk <youssef.afkir@student.odisee.be>
2024-12-04 17:24:44 +0100

    WPF CHAT

---
+++ SlnLes01VisualStudioEnWpf/Abonnement/Program.cs
@@ -0,0 +1,40 @@
+﻿    namespace Abonnement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Bestgegevens
+================");
+            Console.Write("-naam klant:");
+           string naam = Console.ReadLine();
+           Console.WriteLine("- aantal toegangsbeurten:");
+            string aantal = Console.ReadLine();
+             Console.WriteLine("-prijs(b.v. 122, 5):");
+            string prijs = Console.ReadLine();
+            Console.WriteLine("- badkledij inbegrepen(typ true of false");
+            bool badkledij;
+            Console.WriteLine("-geslacht(druk 'm' of 'v'): ");
+
+
+
+
+
+
+            Console.WriteLine(@$"samenvatting 
+                ================
+
+- houder:{naam};
+- geslacht:    
+- prijs: 
+- aantal beurten: 
+- incl. badkledij: 
+- kaartnummer: ");
+
+
+
+
+
+        }
+    }
+}
