ee02af7cdfa8f6e0555d2ca2d290e25b39834aed
YoussAfk <youssef.afkir@student.odisee.be>
2024-12-11 17:13:20 +0100

    eind cijfer

---
+++ SlnLes01VisualStudioEnWpf/RadenGrenzen/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RadenGrenzen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+__________    _____  ________  ___________ _______   
+\______   \  /  _  \ \______ \ \_   _____/ \      \  
+ |       _/ /  /_\  \ |    |  \ |    __)_  /   |   \ 
+ |    |   \/    |    \|    `   \|        \/    |    \   
+ |____|_  /\____|__  /_______  /_______  /\____|__  /
+        \/         \/        \/        \/         \/ ");
+
+            Console.WriteLine();
+            
+            int gehele = 
+            Console.ReadLine("Geef twee gehele getalen.");
+            Console.WriteLine(");
+            Console.WriteLine();
+
+        }
+    }
+}
